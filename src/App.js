573b20d967ea945a2b6467bcd101defc5337857f
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-05 19:33:40 +0100

    added footer with link to github repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
       Weather App
+      <footer>This project was coded by <a href="https://github.com/megj88/react-weather-search" target="_blank">Megan Jones</a> and is open-sourced.</footer>
+      
       </header>
     </div>
   );

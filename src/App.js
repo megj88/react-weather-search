2dfeb4a4fdb40137a9af3b026ef2e2176019c1b1
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-07 11:23:47 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Weather App
+      <div className="container">
+
+      <h1>Weather App</h1>
       <footer>This project was coded by <a href="https://github.com/megj88/react-weather-search" target="_blank">Megan Jones</a> and is open-sourced.</footer>
       
-      </header>
+    </div>
     </div>
   );
 }

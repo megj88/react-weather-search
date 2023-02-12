10adc455edc4a9cef7d42a2136658b2f88625bf0
Megan Jones <115540207+megj88@users.noreply.github.com>
2023-02-12 09:34:27 +0100

    fix link target blank

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="container">
 
       <Weather defaultCity="Bari" />
-      <footer>This project was coded by <a href="https://github.com/megj88/react-weather-search" target="_blank">Megan Jones</a> and is open-sourced.</footer>
+      <footer>This project was coded by <a href="https://github.com/megj88/react-weather-search" target="_blank" rel="noopener noreferrer">Megan Jones</a> and is open-sourced.</footer>
       
     </div>
     </div>
